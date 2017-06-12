db37afe12c03db03776e1889f6393dbd56da50ee
gouzhiqiang <zhiqiang.gou@ceeyoo.net>
2017-06-12 19:02:34 +0800

    import JSON

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LitJson;
 
 public class Test : MonoBehaviour {
 
+    public Player Data = new Player();
+    JsonWriter da = new JsonWriter();
     // Use this for initialization
     void Start() {
+        da.WriteObjectStart();
+        da.WritePropertyName("NAME");
+        da.Write("gzq");
+        da.WritePropertyName("AGE");
+        da.Write(12);
+        da.WriteObjectEnd();
 
     }
 
@@ -15,9 +24,21 @@ public class Test : MonoBehaviour {
     }
     void OnGUI()
     {
-        if (GUI.Button(new Rect(10,10,100,100),"ss"))
+        if (GUI.Button(new Rect(10, 10, 100, 100), "ss"))
         {
+            string res = da.ToString();
+         
+            Data = LitJson.JsonMapper.ToObject<Player>(res);
+            Debug.Log(Data.NAME);
+            Debug.Log(Data.AGE);
+            Debug.Log(res);
+            
             
         }
     }
+}
+public class Player
+{
+    public string NAME;
+    public int AGE = 0; 
 }
